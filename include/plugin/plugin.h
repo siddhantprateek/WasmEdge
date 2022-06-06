c1f72e75a7aa0246bb8b1a7919fc57c49f07a751
Shen-Ta Hsieh <beststeve@secondstate.io>
2022-06-06 18:29:27 +0800

    [Plugin] Add plugin in c interface
    Signed-off-by: Shen-Ta Hsieh <beststeve@secondstate.io>

--- include/plugin/plugin.h
+++ include/plugin/plugin.h
@@ -35,7 +35,8 @@ public:
   struct ModuleDescriptor {
     const char *Name;
     const char *Description;
-    Runtime::Instance::ModuleInstance *(*Create)(void) noexcept;
+    Runtime::Instance::ModuleInstance *(*Create)(
+        const ModuleDescriptor *) noexcept;
   };
 
   constexpr PluginModule() noexcept {}
@@ -52,7 +53,8 @@ public:
 
   std::unique_ptr<Runtime::Instance::ModuleInstance> create() const noexcept {
     assuming(Desc);
-    return std::unique_ptr<Runtime::Instance::ModuleInstance>(Desc->Create());
+    return std::unique_ptr<Runtime::Instance::ModuleInstance>(
+        Desc->Create(Desc));
   }
 
 private:
@@ -78,10 +80,11 @@ public:
     VersionData Version;
     size_t ModuleCount;
     PluginModule::ModuleDescriptor *ModuleDescriptions;
-    void (*AddOptions)(PO::ArgumentParser &Parser) noexcept;
+    void (*AddOptions)(const PluginDescriptor *D,
+                       PO::ArgumentParser &Parser) noexcept;
   };
 
-  static inline constexpr const uint32_t CurrentAPIVersion [[maybe_unused]] = 1;
+  static inline constexpr const uint32_t CurrentAPIVersion [[maybe_unused]] = 2;
   static std::vector<std::filesystem::path> getDefaultPluginPaths() noexcept;
   static bool load(const std::filesystem::path &Path) noexcept;
   static void addPluginOptions(PO::ArgumentParser &Parser) noexcept;
@@ -113,7 +116,7 @@ public:
   void registerOptions(PO::ArgumentParser &Parser) const noexcept {
     assuming(Desc);
     if (Desc->AddOptions) {
-      Desc->AddOptions(Parser);
+      Desc->AddOptions(Desc, Parser);
     }
   }
 
